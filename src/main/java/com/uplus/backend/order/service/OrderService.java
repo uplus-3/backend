96c8ed2dd886131e5717ebc4fef3a31e34ff6f45
suhyeon <kim113355@naver.com>
2022-09-01 16:47:18 +0900

    :sparkles: Order 수정, 삭제 API 구현 (#44)

--- src/main/java/com/uplus/backend/order/service/OrderService.java
+++ src/main/java/com/uplus/backend/order/service/OrderService.java
@@ -12,12 +12,14 @@ import com.uplus.backend.global.util.OrderNumberUtil;
 import com.uplus.backend.order.dto.OrderCreateRequestDto;
 import com.uplus.backend.order.dto.OrderCreateResponseDto;
 import com.uplus.backend.order.dto.OrderResponseDto;
+import com.uplus.backend.order.dto.OrderUpdateRequestDto;
 import com.uplus.backend.order.entity.Order;
 import com.uplus.backend.order.repository.OrderRepository;
 import com.uplus.backend.plan.entity.Plan;
 import com.uplus.backend.plan.repository.PlanRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,56 +28,76 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class OrderService {
 
-	private final OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
 
-	private final ColorRepository colorRepository;
+    private final ColorRepository colorRepository;
 
-	private final PlanRepository planRepository;
+    private final PlanRepository planRepository;
 
-	@Transactional
-	public OrderCreateResponseDto create(OrderCreateRequestDto orderCreateRequestDto) {
+    @Transactional
+    public OrderCreateResponseDto create(OrderCreateRequestDto orderCreateRequestDto) {
 
-		// TODO : COLOR_NO_DATA_ERROR Test 코드 작성
-		Color color = colorRepository.findById(orderCreateRequestDto.getColorId())
-			.orElseThrow(() -> new CustomException(COLOR_NO_DATA_ERROR));
+        // TODO : COLOR_NO_DATA_ERROR Test 코드 작성
+        Color color = colorRepository.findById(orderCreateRequestDto.getColorId())
+            .orElseThrow(() -> new CustomException(COLOR_NO_DATA_ERROR));
 
-		// TODO : PLAN_NO_DATA_ERROR Test 코드 작성
-		Plan plan = planRepository.findById(orderCreateRequestDto.getPlanId())
-			.orElseThrow(() -> new CustomException(PLAN_NO_DATA_ERROR));
+        // TODO : PLAN_NO_DATA_ERROR Test 코드 작성
+        Plan plan = planRepository.findById(orderCreateRequestDto.getPlanId())
+            .orElseThrow(() -> new CustomException(PLAN_NO_DATA_ERROR));
 
-		// 재고 확인
-		if (color.getStock() > 0) {
-			color.setStock(color.getStock() - 1);
-			colorRepository.save(color);
-		} else {
-			// TODO : NO_STOCK_ERROR Test 코드 작성
-			throw new CustomException(NO_STOCK_ERROR);
-		}
+        // 재고 확인
+        if (color.getStock() > 0) {
+            color.setStock(color.getStock() - 1);
+            colorRepository.save(color);
+        } else {
+            // TODO : NO_STOCK_ERROR Test 코드 작성
+            throw new CustomException(NO_STOCK_ERROR);
+        }
 
-		// 주문번호 부여
-		Long orderNumber;
-		boolean checkOrderNumberDuplicated;
-		do {
-			orderNumber = OrderNumberUtil.createOrderNumber();
-			checkOrderNumberDuplicated = orderRepository.existsByNumber(
-				OrderNumberUtil.createOrderNumber());
-		} while (checkOrderNumberDuplicated);
+        // 주문번호 부여
+        Long orderNumber;
+        boolean checkOrderNumberDuplicated;
+        do {
+            orderNumber = OrderNumberUtil.createOrderNumber();
+            checkOrderNumberDuplicated = orderRepository.existsByNumber(
+                OrderNumberUtil.createOrderNumber());
+        } while (checkOrderNumberDuplicated);
 
-		Order order = orderCreateRequestDto.toEntity(color, plan,
-			orderNumber);
+        Order order = orderCreateRequestDto.toEntity(color, plan,
+            orderNumber);
 
-		orderRepository.save(order);
+        orderRepository.save(order);
 
-		return OrderCreateResponseDto.fromEntity(order);
-	}
+        return OrderCreateResponseDto.fromEntity(order);
+    }
 
-	@Transactional(readOnly = true)
-	public OrderResponseDto getByNameAndNumber(String name, Long number) {
+    @Transactional(readOnly = true)
+    public OrderResponseDto getByNameAndNumber(String name, Long number) {
 
-		// TODO : ORDER_NO_DATA_ERROR Test 코드 작성
-		Order order = orderRepository.findByNameAndNumber(name, number)
-			.orElseThrow(() -> new CustomException(ORDER_NO_DATA_ERROR));
+        // TODO : ORDER_NO_DATA_ERROR Test 코드 작성
+        Order order = orderRepository.findByNameAndNumber(name, number)
+            .orElseThrow(() -> new CustomException(ORDER_NO_DATA_ERROR));
 
-		return OrderResponseDto.fromEntity(order);
-	}
+        return OrderResponseDto.fromEntity(order);
+    }
+
+    @Transactional
+    public OrderResponseDto update(Long orderId, OrderUpdateRequestDto orderUpdateRequestDto) {
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow(() -> new CustomException(ORDER_NO_DATA_ERROR));
+
+        order.setAddress(orderUpdateRequestDto.getAddress());
+        order = orderRepository.save(order);
+
+        return OrderResponseDto.fromEntity(order);
+    }
+
+    @Transactional
+    public void delete(Long orderId) {
+        try {
+            orderRepository.deleteById(orderId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new CustomException(ORDER_NO_DATA_ERROR);
+        }
+    }
 }
