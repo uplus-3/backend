7bd5e2eb32b2383320147c76990b4f0afd37b57a
SuHyeon Kim <kim113355@naver.com>
2022-09-03 22:25:57 +0900

    :recycle: Order 수정 메서드 변경

--- src/main/java/com/uplus/backend/order/controller/OrderController.java
+++ src/main/java/com/uplus/backend/order/controller/OrderController.java
@@ -14,15 +14,7 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 주문 관련 API 요청 처리를 위한 컨트롤러 정의.
@@ -66,7 +58,7 @@ public class OrderController {
         return ResponseEntity.ok().body(orderResponseDto);
     }
 
-    @PatchMapping("/{order-id}")
+    @PutMapping("/{order-id}")
     @ApiOperation(value = "주문 수정", notes = "전송 받은 주문 번호에 해당하는 주문 데이터를 전송 받은 데이터로 수정합니다.")
     @ApiResponses({
         @ApiResponse(code = 200, message = "주문 수정 성공"),
